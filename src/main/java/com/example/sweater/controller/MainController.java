ad733e61be956e0b919f42f9aa39a42ef04f618d
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2020-02-20 14:56:25 +0300

    Added spring-security dependency New entities and a lot of new features

---
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -0,0 +1,59 @@
+package com.example.sweater.controller;
+
+
+import com.example.sweater.domain.Message;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller//Это контроллер который управляет всеми CRUD операциями с базами данных
+public class MainController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
+        return "greeting";// Возвращает файл-шаблон
+    }
+
+    @GetMapping("/main")//Эта аннотация необходима для получения из баз данных
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);//Создаём новый объект типа Message
+
+        messageRepo.save(message);//Сохраняем его
+
+        Iterable<Message> messages = messageRepo.findAll();//Взяли его из репозитория
+
+        model.put("messages", messages);//И положили в модель для отображения
+
+        return "main";//Возвращает модель main.mustache из папки templates
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "main";
+    }
+}

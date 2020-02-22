3296325cb4d981a395490cd82826682b5de878fe
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2020-02-22 14:36:55 +0300

    some visual fixes

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -18,13 +18,13 @@ import java.util.stream.Collectors;
 @RequestMapping("/user")
 @PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
-
     @Autowired
     private UserRepo userRepo;
 
     @GetMapping
     public String userList(Model model) {
         model.addAttribute("users", userRepo.findAll());
+
         return "userList";
     }
 
@@ -33,24 +33,22 @@ public class UserController {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
 
-        return "userEdit";//Возвращаем шаблон
+        return "userEdit";
     }
 
-    //Добавляем нового пользлвателя
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
-            @RequestParam("userId") User user) {
-
+            @RequestParam("userId") User user
+    ) {
         user.setUsername(username);
 
-        user.getRoles().clear();
-
-        Set<String> roles = Arrays
-                .stream(Role.values())
+        Set<String> roles = Arrays.stream(Role.values())
                 .map(Role::name)
                 .collect(Collectors.toSet());
+
+        user.getRoles().clear();
 
         for (String key : form.keySet()) {
             if (roles.contains(key)) {
@@ -59,6 +57,7 @@ public class UserController {
         }
 
         userRepo.save(user);
+
         return "redirect:/user";
     }
 }

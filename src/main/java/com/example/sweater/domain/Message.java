7571ca344d3cb4aa5f00ff81614d36533b1a0368
Sheba1990 <nikitashebin@gmail.com>
2023-05-28 11:09:46 +0300

    Added captcha

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -1,13 +1,20 @@
 package com.example.sweater.domain;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @NotBlank(message = "Please fill this message")
+    @Length(max = 2048, message = "Message too long (more than 2kB)")
     private String text;
+    @Length(max = 255, message = "Tag too long (more than 2kB)")
     private String tag;
 
     @ManyToOne(fetch = FetchType.EAGER)
